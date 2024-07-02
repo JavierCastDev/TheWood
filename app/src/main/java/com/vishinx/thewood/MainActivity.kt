94b0d0c851e7ed0c43bfd786a10cf31cb95bf21c
Javier Castillo Hernández <javier.developer.kt@gmail.com>
2024-07-01 18:15:07 -0600

    Se agrego el boton play. Se agrego la animacion al boton.

--- app/src/main/java/com/vishinx/thewood/MainActivity.kt
+++ app/src/main/java/com/vishinx/thewood/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.vishinx.thewood
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val playImageButton: ImageButton = findViewById(R.id.play_button)
+        setScaleAnimation(playImageButton, this)
 
         mediaPlayer = MediaPlayer.create(this, R.raw.instrumento)
         mediaPlayer.isLooping = true
@@ -39,5 +47,29 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release()
+    }
+}
+
+fun setScaleAnimation(view: View, context: Context) {
+    val scaleUp = AnimationUtils.loadAnimation(context, R.anim.scale_up)
+    val scaleDown = AnimationUtils.loadAnimation(context, R.anim.scale_down)
+
+    view.setOnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                v.startAnimation(scaleDown)
+                v.isPressed = true
+            }
+            MotionEvent.ACTION_UP -> {
+                v.startAnimation(scaleUp)
+                v.performClick()
+                v.isPressed = false
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                v.startAnimation(scaleUp)
+                v.isPressed = false
+            }
+        }
+        true
     }
 }
